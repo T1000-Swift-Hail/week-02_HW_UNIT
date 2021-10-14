13c3c4fdaaa8f5a134fdfb674be3f833415c98dd
موضي الحربي <modhialhrbi@MacBook-Air-mwdy.local>
2021-10-14 16:06:45 +0300

    DN HW UNIT

---
+++ HW_UNIT.playground/Contents.swift
@@ -0,0 +1,64 @@
+import Darwin
+
+protocol Person {
+
+    var name : String { get  }
+    var birthPlace : String{get}
+    var age : Int {get}
+    func walk()
+    func run()
+    func speak()
+}
+enum PaymentError : Error {
+case paymentLow
+}
+struct Artist: Person {
+    var name: String
+    var birthPlace :String
+    var age : Int
+    
+    func walk() {
+        print ("Walking...")  }
+    func run(){
+        print ("RUNNING!!")   }
+    func speak(){
+        print ("Speaking...")   }
+    }
+extension Artist{
+ func doPainting(payment : Int)throws -> String{
+        
+        guard payment >= 500 else {
+           
+        throw PaymentError.paymentLow
+        }
+        return "Your Painting is Done."
+}}
+
+var ahmed = Artist(name: "khaled", birthPlace: "taife", age: 40)
+    do {
+        let payment = try ahmed.doPainting(payment: 400)
+        print(payment)}
+catch PaymentError.paymentLow{
+    print("Error")
+}
+
+struct Doctor:Person {
+    var name: String
+    var birthPlace: String
+    var age: Int
+    var specialization : String
+    var description : String {
+        return "my name is \(name) and I am from \(birthPlace)and  My age is\(age) and my specialty is\(specialization) " }
+        
+    func walk() {
+        print ("Walking...")  }
+    
+    func run() {
+        print ("Doctors don't run") }
+    
+    func speak() {
+        print ("Speaking...")  }
+}
+let Nora = Doctor(name: "NORA", birthPlace: "khobar", age: 36, specialization: "Internal" )
+    
+print(Nora.description)
