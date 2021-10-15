6d60b776fff74d2fc58e53a944004efa06dae921
Hind Aljehani <Hind.Aljehani.has.s@tuwaiq.edu.sa>
2021-10-15 15:37:32 +0300

    done hw

--- README.playground/Contents.swift
+++ README.playground/Contents.swift
@@ -1,23 +1,30 @@
 import UIKit
 import Darwin
-protocol persona {
+protocol person {
 
     var name :String{get}
-    var birthplace: String{get}
+    var birthplace: String {get}
     var age : Int{get}
     func walk()
     func run ()
     func speak()
 }
-enum paymentError{
+enum paymentError :Error{
 
 case paymentLow
     
 }
-class Artist : persona {
+class Artist  :person {
+    
     var name: String
     var birthplace: String
     var age: Int
+    
+    init(n:String, b: String, a :Int){
+        self.name = n
+        self.birthplace = b
+        self.age=a
+    }
     func walk(){
         print("walking")
     }
@@ -27,37 +34,46 @@ class Artist : persona {
     func speak(){
     print("Speaking")
     }
-    init(name:String,birthplace:String,age:Int){
-        self.name = name
-        self.birthplace=birthplace
-        self.age=age
-        }
+        
 }
 extension Artist {
+    
     func dopainting(payment :Int)throws {
   guard payment >= 500 else {
         }
       print("You painting is Done")
     }
-}
-var ArristSalim = Artist(n: "Sa",b:"hail",a: 40)
-do {
-    try ArristSalim.dopainting(payment: 400)
-}
-catch paymentError.paymentLow {
-    print("The payment is low")
-}
-struct Doctor : person{
-    var name: String,birthplace:String,age:Int
-    var specializator : String
-    var descriotor : String {
+   var ArtistHind = Artist(n:"Hd", b: "hail", a: 28)
+    
+  do {
+   try ArtistAhmed.dopainting(payment:400)
     }
-    let Nore = Doctor(name: "Nore",birthplace: "hail",age: 35,specializator: "CS")
-    print (nore.descriptor)
-    
-    
-    
-    
+catch paymentError.paymentLow {
+print("The payment is low")
+}
+struct Doctor : person {
+   var name: String,birthplace:String,age: Int
+        var Specialization : String
+            var descriptor :String {
+   return "M Name is \(name) . I am from \(birthplace).My doctor and my specialty is \(Specialization)"
+            }
+            func Walk() {
+         print("walking")
+            }
+            func run() {
+                print("run")
+            }
+            func speak() {
+                print("speak")
+            
+            }
+let Hind = (name:"Hind",birthplace: "hail", age: 28,Specialization:"CS")
+  print(Hind.descriptor)
+
+
+
+
+
 
 
 
