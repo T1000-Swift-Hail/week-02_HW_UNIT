2d6fe798a45b1f53d4ef4943e875fd8002f2d5d6
Hind Aljehani <Hind.Aljehani.has.s@tuwaiq.edu.sa>
2021-10-17 13:18:44 +0300

    last edit hw

--- README.playground/Contents.swift
+++ README.playground/Contents.swift
@@ -1,10 +1,10 @@
 import UIKit
 import Darwin
-protocol person {
+protocol Person{
 
-    var name :String{get}
+    var name :String {get}
     var birthplace: String {get}
-    var age : Int{get}
+    var age : Int {get}
     func walk()
     func run ()
     func speak()
@@ -14,7 +14,26 @@ enum paymentError :Error{
 case paymentLow
     
 }
-class Artist  :person {
+
+class Artist : Person {
+    
+    func doPainting(payment : Int) throws -> String {
+    guard payment >= 500 else { throw paymentError.paymentLow}
+    return "your painting is Done"
+
+}
+    func walk() {
+        print("Walking")
+    }
+    
+    func run() {
+        print("RUNNing")
+    }
+    
+    func speak() {
+        print("Speaking")
+    }
+    
     
     var name: String
     var birthplace: String
@@ -25,38 +44,26 @@ class Artist  :person {
         self.birthplace = b
         self.age=a
     }
-    func walk(){
-        print("walking")
-    }
-    func run(){
-        print("Running")
-    }
-    func speak(){
-    print("Speaking")
-    }
-        
 }
-extension Artist {
+
+let ArtistAhmed = Artist(n: "Ah",b: "hail",a: 30)
     
-    func dopainting(payment :Int)throws {
-  guard payment >= 500 else {
-        }
-      print("You painting is Done")
-    }
-   var ArtistHind = Artist(n:"Hd", b: "hail", a: 28)
-    
-  do {
-   try ArtistAhmed.dopainting(payment:400)
-    }
+ do {
+     let dopay = try ArtistAhmed.doPainting(payment: 400)
+     print(dopay)
+}
 catch paymentError.paymentLow {
-print("The payment is low")
+    print("The payment is low")
+    
 }
-struct Doctor : person {
-   var name: String,birthplace:String,age: Int
-        var Specialization : String
-            var descriptor :String {
-   return "M Name is \(name) . I am from \(birthplace).My doctor and my specialty is \(Specialization)"
-            }
+    struct Doctor : Person {
+        func walk() {
+            print("Walking")
+        }
+        
+    var name: String,birthplace: String,age: Int
+    var Specialization : String
+    
             func Walk() {
          print("walking")
             }
@@ -67,10 +74,17 @@ struct Doctor : person {
                 print("speak")
             
             }
-let Hind = (name:"Hind",birthplace: "hail", age: 28,Specialization:"CS")
-  print(Hind.descriptor)
+        var descriptor : String {
+        
+        return "My Name is \(name).I am from \(birthplace). My doctor and my specialty is \(Specialization)"
 
-
+        }
+        
+    }
+let Nore = Doctor(name: "Nora",birthplace: "hail", age: 28,Specialization:"CS")
+  print (Nore.descriptor)
+    
+    
 
 
 
